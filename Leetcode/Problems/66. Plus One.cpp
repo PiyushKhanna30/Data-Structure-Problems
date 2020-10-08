f2dc139c3803a087ae5c64d5af097d4323f33176
PiyushKhanna30 <khanna.piyush30@gmail.com>
2020-10-08 20:20:01 +0530

    66 - updated

--- Leetcode/Problems/66. Plus One.cpp
+++ Leetcode/Problems/66. Plus One.cpp
@@ -1,26 +1,54 @@
+/*
+
+Approach 1:
+start from last and set carry =1. with each iteration change digit at the index. at last if carry exist then copy to new vector.
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n=digits.size();            //Size of vector
-        vector<int>ans(n+1,0);          //Result vector of size +1 for case like 9+1=10
-        int carry=1;                    //Since +1 so got carry =1
-        for(int i=n-1;i>=0;i--)         //loop thrugh digits add carry and put in ans
+        int carry = 1;
+        for(int i=digits.size()-1;i>=0;i--)
         {
-            int t=0;
-            t=carry+digits[i];
-            int m=t/10;
-            t=t%10;
-            ans[i+1]=t;
-            carry=m;
+            int num = digits[i]+carry;
+            digits[i] = num%10;
+            carry = num/10;
         }
-        if(carry!=0)                    //eg 9+1 carry here 1
+        if(carry != 0)
         {
-            ans[0]=carry;
-            return ans;
+            vector<int>dig;
+            dig.push_back(carry);
+            for(int num : digits)
+                dig.push_back(num);
+            return dig;
         }
-        ans.erase(ans.begin());         //eg 1+1 carry =0 so remve 1st space
-        return ans;
-        
+        return digits;
     }
 };
-
+Memory O(n)
+Space O(n)
+*/
+//Approach 2 Better
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            if(digits[i]==9)            //if digit is 9, so 9+1 = 10 ie 0 and carry 1 or add 1 in i-1
+            {
+                digits[i]=0;
+            }
+            else
+            {
+                digits[i]+=1;           //if digit <9 then +1 will not be greater than or equal to 10 so carry =0 and no need to check prev digits;
+                return digits;
+            }
+        }
+        //eg 9,9,9 -> 0,0,0
+        digits[0]=1;
+        digits.push_back(0);
+        return digits;
+    }
+};
+//Space O(1)
+//Memory O(n)
