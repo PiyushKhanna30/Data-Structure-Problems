e891c2c149987a6057879b46c75deff83703b225
PiyushKhanna30 <khanna.piyush30@gmail.com>
2020-10-26 02:45:37 +0530

    430 update

--- Leetcode/Problems/430. Flatten a Multilevel Doubly Linked List.cpp
+++ Leetcode/Problems/430. Flatten a Multilevel Doubly Linked List.cpp
@@ -12,25 +12,30 @@ public:
 class Solution {
 public:
     Node *t;
-    void help(Node *h) 
+    void flattenHelp(Node *head)
     {
-        if(h!=NULL) 
-        {
-           t->next=new Node(h->val) ;
-           Node *q=t;
-           t=t->next;
-           t->prev=q;
-           help(h->child) ;
-           help(h->next) ;
-        }
+        if(!head)return;
+        
+        Node *m = t;
+        t->next = new Node(head->val);
+        t=t->next;
+        t->prev = m;
+
+        flattenHelp(head->child);
+        flattenHelp(head->next);
+        return;
     }
     Node* flatten(Node* head) {
-        Node *root=new Node(5) ;
-        t=root;
-        help(head);
-        root=root->next;
-        if(root) 
-            root->prev=NULL;
-        return root;
+        if(!head)return head;
+        Node *res = new Node(0);
+        t = res;
+        flattenHelp(head);
+        res=res->next;
+        res->prev = NULL;
+        return res;
     }
 };
+//Note 
+// we cant here just use same nodes as we keep on updating next
+// pointer so we create node a in when coming back from recursion
+// step we need to go to old next node.
