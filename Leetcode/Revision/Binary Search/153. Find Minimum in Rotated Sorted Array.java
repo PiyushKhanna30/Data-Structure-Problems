2f28f5d9d78bfa2cbfde0aaec98d1c7fba12db3f
Piyush Khanna <khanna.piyush30@gmail.com>
2024-05-19 14:02:50 +0530

    Update 153. Find Minimum in Rotated Sorted Array.java

--- Leetcode/Revision/Binary Search/153. Find Minimum in Rotated Sorted Array.java
+++ Leetcode/Revision/Binary Search/153. Find Minimum in Rotated Sorted Array.java
@@ -2,21 +2,44 @@ class Solution {
     public int findMin(int[] nums) {
         int start = 0;
         int end = nums.length-1;
-        
+        int ans = Integer.MAX_VALUE;
         while(start<=end){
-            int mid = start + (end-start)/2;
-            //just left number must be greater than smallest element
-            if(mid>0 && nums[mid-1]>nums[mid])
-                return nums[mid];
-            
-            //check if answere lies in right side by check if cur is highest from both sides
-            if(nums[start]<=nums[mid] && nums[mid]>nums[end]){
+            int mid = start + (end - start)/2;
+            if(nums[start]<=nums[end]){
+                ans = Math.min(nums[start], ans);
+                return ans;
+            }
+            else if(nums[start]<=nums[mid]){
+                ans = Math.min(nums[start], ans);
                 start = mid + 1;
             }
             else{
-                end = mid - 1;
+                ans = Math.min(nums[mid], ans);
+                end = mid-1;
             }
         }
-        return nums[0];
+        return ans;
     }
 }
+// class Solution {
+//     public int findMin(int[] nums) {
+//         int start = 0;
+//         int end = nums.length-1;
+        
+//         while(start<=end){
+//             int mid = start + (end-start)/2;
+//             //just left number must be greater than smallest element
+//             if(mid>0 && nums[mid-1]>nums[mid])
+//                 return nums[mid];
+            
+//             //check if answere lies in right side by check if cur is highest from both sides
+//             if(nums[start]<=nums[mid] && nums[mid]>nums[end]){
+//                 start = mid + 1;
+//             }
+//             else{
+//                 end = mid - 1;
+//             }
+//         }
+//         return nums[0];
+//     }
+// }
